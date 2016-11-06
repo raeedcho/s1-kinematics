1de7bff8562a31d20f6ee8e0803159c99a08ce46
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2016-11-06 01:26:40 -0600

    refactoring code to use more binnedData class (still unfinished)

--- load_cds_multiworkspace.m
+++ load_cds_multiworkspace.m
@@ -6,8 +6,8 @@ ranBy='ranByRaeed';
 monkey='monkeyHan';
 task='taskRW';
 array='arrayLeftS1Area2';
-% folder='C:\Users\Raeed\Projects\limblab\data-raeed\MultiWorkspace\SplitWS\Han\20160322\area2\preCDS\';
-folder = '/home/raeed/Projects/limblab/data-raeed/MultiWorkspace/SplitWS/Han/20160322/area2/preCDS/';
+folder='C:\Users\Raeed\Projects\limblab\data-raeed\MultiWorkspace\SplitWS\Han\20160322\area2\preCDS\';
+% folder = '/home/raeed/Projects/limblab/data-raeed/MultiWorkspace/SplitWS/Han/20160322/area2/preCDS/';
 DL_fname='Han_20160322_RW_DL_area2_001';
 PM_fname='Han_20160322_RW_PM_area2_002';
 mapfile='mapFileC:\Users\Raeed\Projects\limblab\data-raeed\MultiWorkspace\SplitWS\Han\mapfile\left_S1\SN 6251-001459.cmp';
@@ -21,7 +21,7 @@ PM_cds = commonDataStructure();
 PM_cds.file2cds([folder PM_fname],ranBy,array,monkey,lab,'ignoreJumps',task,mapfile);
 
 %% Save CDS files
-% savepath = 'C:\Users\Raeed\Projects\limblab\data-raeed\MultiWorkspace\SplitWS\Han\20160322\area2\CDS\';
-savepath = '/home/raeed/Projects/limblab/data-raeed/MultiWorkspace/SplitWS/Han/20160322/area2/CDS/';
+savepath = 'C:\Users\Raeed\Projects\limblab\data-raeed\MultiWorkspace\SplitWS\Han\20160322\area2\CDS\';
+% savepath = '/home/raeed/Projects/limblab/data-raeed/MultiWorkspace/SplitWS/Han/20160322/area2/CDS/';
 save([savepath DL_fname '_cds.mat'],'DL_cds')
 save([savepath PM_fname '_cds.mat'],'PM_cds')
