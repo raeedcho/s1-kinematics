1d6b03ec12d4cd88a19fae7071a547e37082d5b9
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2018-01-03 17:46:33 -0600

    remove all multiworkspace archive functions

--- Archive/Deprecated code/extract_behaviors.m
+++
@@ -1,5 +0,0 @@
-function [behaviors_new] = extract_behaviors(behaviors,times)
-% Get new behaviors struct with arm data subsampled from original behaviors
-% between times(i,1) and times(i,2). Meant to be used after
-% extract_workspace_times to extract a specific workspace of reaches.
-
--- Archive/remove_wrapping.m
+++
@@ -1,8 +0,0 @@
-function unwrapped = remove_wrapping(wrapped)
-% Function to remove wrapping from subtracting two angles
-
-wrapped(wrapped>pi) = wrapped(wrapped>pi)-2*pi;
-wrapped(wrapped<-pi) = wrapped(wrapped<-pi)+2*pi;
-unwrapped = wrapped;
-
-end
