93b9b201e1dd3ee307d597a1bb7a5bf802bcc7b5
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2018-02-22 14:39:24 -0600

    add new version of analyzeTRT

--- analyzeTRT.m
+++ analyzeTRT.m
@@ -115,178 +115,6 @@
     td_dl_test = getModel(td_dl_test,glm_info_musc);
     td_pm_test = getModel(td_pm_test,glm_info_musc);
 
-%% Get PDs for the modeled and actual neurons
-    num_boots = 100;
-    pd_ext_params = struct('num_boots',num_boots,'out_signals',{{'glm_ext_model'}},'out_signal_names',td(1).S1_unit_guide,'disp_times',true);
-    pd_ego_params = struct('num_boots',num_boots,'out_signals',{{'glm_ego_model'}},'out_signal_names',td(1).S1_unit_guide,'disp_times',true);
-    pd_musc_params = struct('num_boots',num_boots,'out_signals',{{'glm_musc_model'}},'out_signal_names',td(1).S1_unit_guide,'disp_times',true);
-    pd_real_params = struct('num_boots',num_boots,'out_signals',{{'S1_spikes'}},'out_signal_names',td(1).S1_unit_guide,'disp_times',true);
-    pm_ext_pdTable = getTDPDs(td_pm_test,pd_ext_params);
-    pm_ego_pdTable = getTDPDs(td_pm_test,pd_ego_params);
-    pm_musc_pdTable = getTDPDs(td_pm_test,pd_musc_params);
-    pm_real_pdTable = getTDPDs(td_pm_test,pd_real_params);
-    dl_ext_pdTable = getTDPDs(td_dl_test,pd_ext_params);
-    dl_ego_pdTable = getTDPDs(td_dl_test,pd_ego_params);
-    dl_musc_pdTable = getTDPDs(td_dl_test,pd_musc_params);
-    dl_real_pdTable = getTDPDs(td_dl_test,pd_real_params);
-
-    % get PD shifts
-    ext_pdShift = minusPi2Pi(dl_ext_pdTable.velPD-pm_ext_pdTable.velPD);
-    ego_pdShift = minusPi2Pi(dl_ego_pdTable.velPD-pm_ego_pdTable.velPD);
-    musc_pdShift = minusPi2Pi(dl_musc_pdTable.velPD-pm_musc_pdTable.velPD);
-    real_pdShift = minusPi2Pi(dl_real_pdTable.velPD-pm_real_pdTable.velPD);
-
-    % figure out which neurons are tuned
-    isTuned_ext = checkIsTuned(pm_ext_pdTable) & checkIsTuned(dl_ext_pdTable);
-    isTuned_ego = checkIsTuned(pm_ego_pdTable) & checkIsTuned(dl_ego_pdTable);
-    isTuned_musc = checkIsTuned(pm_musc_pdTable) & checkIsTuned(dl_musc_pdTable);
-    isTuned_real = checkIsTuned(pm_real_pdTable) & checkIsTuned(dl_real_pdTable);
-
-%% Bootstrap on PD shifts
-    model_names = {'glm_ext_model','glm_ego_model','glm_musc_model','S1_spikes'};
-    num_internal_boots = 1;
-    num_outer_boots = 100;
-    shift_tables = cell(length(model_names),1);
-    trial_idx = randi(length(td_pm_test),length(td_pm_test),num_outer_boots);
-    tic
-    for bootctr = 1:num_outer_boots
-        disp(['Bootstrap sample ' num2str(bootctr) ', starting at ' num2str(toc) 's'])
-        for modelctr = 1:length(model_names)
-            pd_params = struct('num_boots',num_internal_boots,'out_signals',{model_names(modelctr)},'out_signal_names',td(1).S1_unit_guide);
-
-            pm_pdTable = getTDPDs(td_pm_test(trial_idx(:,bootctr)),pd_params);
-            dl_pdTable = getTDPDs(td_dl_test(trial_idx(:,bootctr)),pd_params);
-
-            % compose shift table for this model/bootstrap sample
-            temp_shift_table = pm_pdTable;
-            temp_shift_table.velPD = minusPi2Pi(dl_pdTable.velPD-pm_pdTable.velPD);
-            temp_shift_table.velPDCI = minusPi2Pi(dl_pdTable.velPDCI-pm_pdTable.velPDCI); % this doesn't actually mean anything with one internal boot sample
-            % don't care about moddepth currently
-
-            if bootctr == 1
-                % slot new table into cell array
-                shift_tables{modelctr} = temp_shift_table;
-            else
-                % append to old table
-                shift_tables{modelctr} = [shift_tables{modelctr};temp_shift_table];
-            end
-
-        end
-    end
-
-%% Plot DL vs PM just for neurons actually tuned to velocity
-    figure
-    comparePDs(pm_real_pdTable(isTuned_real,:),dl_real_pdTable(isTuned_real,:),struct('move_corr','vel'),'ko','linewidth',2)
-    hold on
-    comparePDs(pm_ext_pdTable(isTuned_real,:),dl_ext_pdTable(isTuned_real,:),struct('move_corr','vel'),'ro','linewidth',2)
-    comparePDs(pm_ego_pdTable(isTuned_real,:),dl_ego_pdTable(isTuned_real,:),struct('move_corr','vel'),'go','linewidth',2)
-    comparePDs(pm_musc_pdTable(isTuned_real,:),dl_musc_pdTable(isTuned_real,:),struct('move_corr','vel'),'bo','linewidth',2)
-    xlabel 'PM preferred direction'
-    ylabel 'DL preferred direction'
-    
-%% Plot PDs against real PD shift (one point per bootstrap shift)
-    isTuned_rep = repmat(isTuned_real,num_outer_boots,1);
-    figure
-    comparePDs(shift_tables{4}(isTuned_rep,:),shift_tables{1}(isTuned_rep,:),struct('move_corr','vel'),'ro','linewidth',2)
-    hold on
-    comparePDs(shift_tables{4}(isTuned_rep,:),shift_tables{2}(isTuned_rep,:),struct('move_corr','vel'),'go','linewidth',2)
-    comparePDs(shift_tables{4}(isTuned_rep,:),shift_tables{3}(isTuned_rep,:),struct('move_corr','vel'),'bo','linewidth',2)
-    xlabel 'Actual PD shift'
-    ylabel 'Modeled PD shift'
-    title 'One point per bootstrap sample'
-
-%% Plot PD shift clouds for each neuron individually
-    tic;
-    comparePDClouds(shift_tables{4},shift_tables{1},struct('filter_tuning',1),'r','facealpha',0.5)
-    comparePDClouds(shift_tables{4},shift_tables{2},struct('filter_tuning',1),'g','facealpha',0.5)
-    comparePDClouds(shift_tables{4},shift_tables{3},struct('filter_tuning',1),'b','facealpha',0.5)
-    toc
-    % tic
-    % % Loop through and get mean shifts out of bootstrapped shifts
-    % signalIDs = unique(shift_tables{1}.signalID,'rows');
-    % model_cstrings = {'r','g','b'}
-    % model_figures = {figure;figure;figure};
-    % model_titles = {'Hand-based','Egocentric','Muscle-based'}
-    % for modelctr = 1:length(shift_tables)-1
-    %     % set up figure axes
-    %     figure(model_figures{modelctr})
-    %     plot([-pi pi],[0 0],'-k','linewidth',2)
-    %     hold on
-    %     plot([0 0],[-pi pi],'-k','linewidth',2)
-    %     plot([-pi pi],[-pi pi],'--k','linewidth',2)
-    %     axis equal
-    %     set(gca,'box','off','tickdir','out','xtick',[-pi pi],'ytick',[-pi pi],'xlim',[-pi pi],'ylim',[-pi pi],...
-    %         'xticklabel',{'-\pi','\pi'},'yticklabel',{'-\pi','\pi'})
-    %     xlabel 'Actual PD Shift'
-    %     ylabel 'Modeled PD Shift'
-    %     title([model_titles{modelctr} ' model PD shift vs Actual PD shift'])
-    % end
-    % for i = 1:size(signalIDs,1)
-    %     % make new table set
-    %     shift_tables_unit = cell(size(shift_tables));
-    %     for modelctr = 1:length(shift_tables)
-    %         % Populate new table with only that unit's PD shifts
-    %         unit_idx = ismember(shift_tables{modelctr}.signalID,signalIDs(i,:),'rows');
-    %         shift_tables_unit{modelctr} = shift_tables{modelctr}(unit_idx,:);
-    %     end
-    %     % now create hulls and plot
-    %     for modelctr = 1:length(shift_tables)-1
-    %         figure(model_figures{modelctr})
-
-    %         % first plot cloud with center point
-    %         %comparePDs(shift_tables_unit{4},shift_tables_unit{modelctr},struct('move_corr','vel'),[model_cstrings{modelctr} 'o'],'linewidth',2)
-    %         %plot(circ_mean(shift_tables_unit{4}.velPD),circ_mean(shift_tables_unit{modelctr}.velPD),'ko','linewidth',2);
-
-    %         % figure out 95% confidence interval by 'euclidean' distance to circular mean
-    %         % should I be calculating geodesic distance somehow? seems like no, based on a preliminary google search
-    %         clust = [shift_tables_unit{4}.velPD shift_tables_unit{modelctr}.velPD];
-    %         means = [circ_mean(shift_tables_unit{4}.velPD) circ_mean(shift_tables_unit{modelctr}.velPD)];
-    %         centered_clust = minusPi2Pi(clust-repmat(means,size(clust,1),1));
-
-    %         % check whether to plot cluster
-    %         % skip cluster if range is above CI_thresh
-    %         if diff(prctile(centered_clust(:,1),[2.5 97.5]))>pi/3
-    %             continue
-    %         end
-
-    %         dists = sqrt(sum(centered_clust.^2,2));
-    %         inliers = dists<prctile(dists,95);
-    %         clust = clust(inliers,:);
-    %         centered_clust = centered_clust(inliers,:);
-
-    %         % generate convex hull of inliers
-    %         hull_idx = convhull(centered_clust);
-
-    %         % plot cloud
-    %         % plot hull (have to figure out what to do about wraparound)
-    %         patch(centered_clust(hull_idx,1)+means(1),centered_clust(hull_idx,2)+means(2),model_cstrings{modelctr},'facealpha',0.5);
-    %         patch(centered_clust(hull_idx,1)+means(1)-2*pi,centered_clust(hull_idx,2)+means(2),model_cstrings{modelctr},'facealpha',0.5);
-    %         patch(centered_clust(hull_idx,1)+means(1),centered_clust(hull_idx,2)+means(2)-2*pi,model_cstrings{modelctr},'facealpha',0.5);
-    %         patch(centered_clust(hull_idx,1)+means(1)-2*pi,centered_clust(hull_idx,2)+means(2)-2*pi,model_cstrings{modelctr},'facealpha',0.5);
-    %     end
-    % end
-    % toc
-
-%% Plot PDs against each other (one point per mean PD shift)
-    figure
-    % axes
-    plot([-pi pi],[0 0],'-k','linewidth',2)
-    hold on
-    plot([0 0],[-pi pi],'-k','linewidth',2)
-    % unity line
-    plot([-pi pi],[-pi pi],'--k','linewidth',2)
-    % first plot ext vs. real
-    plot(real_pdShift(isTuned_real),ext_pdShift(isTuned_real),'ro','linewidth',3,'markersize',5)
-    % then ego vs real
-    plot(real_pdShift(isTuned_real),ego_pdShift(isTuned_real),'go','linewidth',3,'markersize',5)
-    % then musc vs real
-    plot(real_pdShift(isTuned_real),musc_pdShift(isTuned_real),'bo','linewidth',3,'markersize',5)
-    set(gca,'box','off','tickdir','out','xlim',[-pi pi],'ylim',[-pi pi],'xtick',[-pi 0 pi],'ytick',[-pi 0 pi])
-    axis equal
-    xlabel 'Actual PD shift'
-    ylabel 'Modeled PD shift'
-    title 'One point per mean PD shift'
-
 %% Evaluate model fits
     eval_params = glm_info_ext;
     eval_params.eval_metric = 'pr2';
@@ -306,7 +134,7 @@
     td_musc_pm_eval = squeeze(evalModel(td_pm_test,eval_params));
     td_musc_eval = squeeze(evalModel([td_dl_test td_pm_test],eval_params))
 
-%% Plot example
+%% Plot example neuron model fit
     % neuron 1 has decent pseudo R2
     figure
     for neuron_idx = 1:length(td_ext_dl_eval)
@@ -371,3 +199,117 @@
     set(gca,'box','off','tickdir','out','xlim',[-0.1 0.6],'ylim',[-0.1 0.6])
     xlabel 'Hand-based pR2'
     ylabel 'Muscle-based pR2'
+
+%% Get PDs and tuning curves for the modeled and actual neurons
+    num_boots = 1000;
+    pdTables = cell(2,4); % PM is first row, DL is second. Column order is Ext, Ego, Musc, Real
+    pd_ext_params = struct('num_boots',num_boots,'out_signals',{{'glm_ext_model'}},'out_signal_names',td(1).S1_unit_guide,'disp_times',true);
+    pd_ego_params = struct('num_boots',num_boots,'out_signals',{{'glm_ego_model'}},'out_signal_names',td(1).S1_unit_guide,'disp_times',true);
+    pd_musc_params = struct('num_boots',num_boots,'out_signals',{{'glm_musc_model'}},'out_signal_names',td(1).S1_unit_guide,'disp_times',true);
+    pd_real_params = struct('num_boots',num_boots,'out_signals',{{'S1_spikes'}},'out_signal_names',td(1).S1_unit_guide,'disp_times',true);
+    pdTables{1,1} = getTDPDs(td_pm_test,pd_ext_params);
+    pdTables{1,2} = getTDPDs(td_pm_test,pd_ego_params);
+    pdTables{1,3} = getTDPDs(td_pm_test,pd_musc_params);
+    pdTables{1,4} = getTDPDs(td_pm_test,pd_real_params);
+    pdTables{2,1} = getTDPDs(td_dl_test,pd_ext_params);
+    pdTables{2,2} = getTDPDs(td_dl_test,pd_ego_params);
+    pdTables{2,3} = getTDPDs(td_dl_test,pd_musc_params);
+    pdTables{2,4} = getTDPDs(td_dl_test,pd_real_params);
+
+    % figure out which neurons are tuned
+    isTuned_ext = checkIsTuned(pm_ext_pdTable) & checkIsTuned(dl_ext_pdTable);
+    isTuned_ego = checkIsTuned(pm_ego_pdTable) & checkIsTuned(dl_ego_pdTable);
+    isTuned_musc = checkIsTuned(pm_musc_pdTable) & checkIsTuned(dl_musc_pdTable);
+    isTuned_real = checkIsTuned(pm_real_pdTable) & checkIsTuned(dl_real_pdTable);
+
+    % get tuning curves for all cases
+    num_bins = 8;
+    tuning_curves = cell(2,4); % PM is first row, DL is second. Column order is Ext, Ego, Musc, Real
+    tuning_ext_params = struct('num_bins',num_bins,'out_signals',{{'glm_ext_model'}},'out_signal_names',td(1).S1_unit_guide);
+    tuning_ego_params = struct('num_bins',num_bins,'out_signals',{{'glm_ego_model'}},'out_signal_names',td(1).S1_unit_guide);
+    tuning_musc_params = struct('num_bins',num_bins,'out_signals',{{'glm_musc_model'}},'out_signal_names',td(1).S1_unit_guide);
+    tuning_real_params = struct('num_bins',num_bins,'out_signals',{{'S1_spikes'}},'out_signal_names',td(1).S1_unit_guide);
+    [tuning_curves{1,1},bins] = getTuningCurves(td_pm_test,tuning_ext_params);
+    tuning_curves{2,1} = getTuningCurves(td_dl_test,tuning_ext_params);
+    tuning_curves{1,2} = getTuningCurves(td_pm_test,tuning_ego_params);
+    tuning_curves{2,2} = getTuningCurves(td_dl_test,tuning_ego_params);
+    tuning_curves{1,3} = getTuningCurves(td_pm_test,tuning_musc_params);
+    tuning_curves{2,3} = getTuningCurves(td_dl_test,tuning_musc_params);
+    tuning_curves{1,4} = getTuningCurves(td_pm_test,tuning_real_params);
+    tuning_curves{2,4} = getTuningCurves(td_dl_test,tuning_real_params);
+
+%% Plot comparison of actual tuning curves with various modeled tuning curves
+    % first compare PM and DL tuning for each model
+    figure;compareTuning(tuning_curves(:,1),pdTables(:,1),bins,find(isTuned_real))
+    figure;compareTuning(tuning_curves(:,2),pdTables(:,2),bins,find(isTuned_real))
+    figure;compareTuning(tuning_curves(:,3),pdTables(:,3),bins,find(isTuned_real))
+    figure;compareTuning(tuning_curves(:,4),pdTables(:,4),bins,find(isTuned_real))
+
+    % then compare PM tuning for each model
+    % reorder for color consistency..
+    figure;compareTuning(tuning_curves(1,[3,1,2,4]),pdTables(1,[3,1,2,4]),bins,find(isTuned_real))
+
+    % then compare DL tuning for each model
+    % reorder for color consistency..
+    figure;compareTuning(tuning_curves(2,[3,1,2,4]),pdTables(2,[3,1,2,4]),bins,find(isTuned_real))
+
+%% Plot DL vs PM just for neurons actually tuned to velocity
+    figure
+    comparePDs(pm_real_pdTable(isTuned_real,:),dl_real_pdTable(isTuned_real,:),struct('move_corr','vel'),'ko','linewidth',2)
+    hold on
+    comparePDs(pm_ext_pdTable(isTuned_real,:),dl_ext_pdTable(isTuned_real,:),struct('move_corr','vel'),'ro','linewidth',2)
+    comparePDs(pm_ego_pdTable(isTuned_real,:),dl_ego_pdTable(isTuned_real,:),struct('move_corr','vel'),'go','linewidth',2)
+    comparePDs(pm_musc_pdTable(isTuned_real,:),dl_musc_pdTable(isTuned_real,:),struct('move_corr','vel'),'bo','linewidth',2)
+    xlabel 'PM preferred direction'
+    ylabel 'DL preferred direction'
+    
+%% Bootstrap on PD shifts
+    model_names = {'glm_ext_model','glm_ego_model','glm_musc_model','S1_spikes'};
+    num_internal_boots = 1;
+    num_outer_boots = 1000;
+    shift_tables = cell(length(model_names),1);
+    trial_idx = randi(length(td_pm_test),length(td_pm_test),num_outer_boots);
+    tic
+    for bootctr = 1:num_outer_boots
+        disp(['Bootstrap sample ' num2str(bootctr) ', starting at ' num2str(toc) 's'])
+        for modelctr = 1:length(model_names)
+            pd_params = struct('num_boots',num_internal_boots,'out_signals',{model_names(modelctr)},'out_signal_names',td(1).S1_unit_guide);
+
+            pm_pdTable = getTDPDs(td_pm_test(trial_idx(:,bootctr)),pd_params);
+            dl_pdTable = getTDPDs(td_dl_test(trial_idx(:,bootctr)),pd_params);
+
+            % compose shift table for this model/bootstrap sample
+            temp_shift_table = pm_pdTable;
+            temp_shift_table.velPD = minusPi2Pi(dl_pdTable.velPD-pm_pdTable.velPD);
+            temp_shift_table.velPDCI = minusPi2Pi(dl_pdTable.velPDCI-pm_pdTable.velPDCI); % this doesn't actually mean anything with one internal boot sample
+            % don't care about moddepth currently
+
+            if bootctr == 1
+                % slot new table into cell array
+                shift_tables{modelctr} = temp_shift_table;
+            else
+                % append to old table
+                shift_tables{modelctr} = [shift_tables{modelctr};temp_shift_table];
+            end
+
+        end
+    end
+
+%% Plot PD shift clouds for each neuron individually
+    tic;
+    comparePDClouds(shift_tables{4},shift_tables{1},struct('filter_tuning',1),'r','facealpha',0.5)
+    xlabel 'Actual PD Shift'
+    ylabel 'Modeled PD Shift'
+    title 'Hand-based model PD shift vs Actual PD shift'
+    comparePDClouds(shift_tables{4},shift_tables{2},struct('filter_tuning',1),'g','facealpha',0.5)
+    xlabel 'Actual PD Shift'
+    ylabel 'Modeled PD Shift'
+    title 'Egocentric model PD shift vs Actual PD shift'
+    comparePDClouds(shift_tables{4},shift_tables{3},struct('filter_tuning',1),'b','facealpha',0.5)
+    xlabel 'Actual PD Shift'
+    ylabel 'Modeled PD Shift'
+    title 'Muscle-based model PD shift vs Actual PD shift'
+    toc
+
+%% Find bootstrapped nonparametric shifts
+
