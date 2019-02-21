2c0ad6922ec8c33f819cd763f4516b1bc2d0b85d
Raeed Hasan Chowdhury <raeed.chowdhury@gmail.com>
2019-02-21 14:35:09 -0600

    feat(actpas): add code to extract norm force in plane

--- calculateActpasSeparability.m
+++ calculateActpasSeparability.m
@@ -9,9 +9,9 @@ datadir = fullfile(homefolder,'data','project-data','limblab','s1-kinematics','t
 file_info = dir(fullfile(datadir,'*COactpas*'));
 filenames = horzcat({file_info.name})';
 savedir = fullfile(homefolder,'data','project-data','limblab','s1-kinematics','Results','Separability');
-savesuffix = '_separationResults_allModels_run20190127.mat';
+savesuffix = '_separationResults_1repeat_run20190221.mat';
 
-model_aliases = {'ext','extforce','handle_ext','joint','musc','handelbow'};
+model_aliases = {'ext','extforce','handle_ext','handle_extforce','joint','musc','handelbow'};
 arrayname = 'S1';
 num_musc_pcs = 5;
 
@@ -19,7 +19,7 @@ num_musc_pcs = 5;
 for filenum = 1:length(filenames)
     clear sepResults
 
-    %% load data
+    %% load and preprocess data
     td = load(fullfile(datadir,[filenames{filenum}]));
 
     % rename trial_data for ease
@@ -37,6 +37,14 @@ for filenum = 1:length(filenames)
 
     % get marker velocity
     td = getDifferential(td,struct('signals','markers','alias','marker_vel'));
+    
+    % get speed and ds
+    td = getNorm(td,struct('signals','vel','norm_name','speed'));
+    td = getDifferential(td,struct('signals','speed','alias','dspeed'));
+    
+    % get norm planar force and derivative
+    td = getNorm(td,struct('signals',{{'force',1:2}},'norm_name','plane_force_norm'));
+    td = getDifferential(td,struct('signals','plane_force_norm','alias','dplane_force_norm'));
     
     % remove unsorted neurons
     unit_ids = td(1).S1_unit_guide;
@@ -75,6 +83,11 @@ for filenum = 1:length(filenames)
             'end_name','idx_endTime'));
     [~,td] = getTDidx(td,'result','R');
     td = reorderTDfields(td);
+    
+    % clean nans out...?
+    nanners = isnan(cat(1,td.tgtDir));
+    td = td(~nanners);
+    fprintf('Removed %d trials because of missing target direction\n',sum(nanners))
 
     % remove trials where markers aren't present
     bad_trial = false(length(td),1);
@@ -96,13 +109,14 @@ for filenum = 1:length(filenames)
     td(bad_trial) = [];
     fprintf('Removed %d trials because of missing muscles\n',sum(bad_trial))
     
+    %% find separabilities
     % suppress getTDfields warning...
     getTDfields(td,'time');
     onetime_warn = warning('query','last'); 
     warning('off',onetime_warn.identifier)
     
     sepResults = actpasSep(td,struct(...
-        'num_repeats',20,...
+        'num_repeats',1,...
         'num_folds',5,...
         'model_aliases',{model_aliases},...
         'num_musc_pcs',num_musc_pcs));
@@ -115,40 +129,40 @@ for filenum = 1:length(filenames)
 end
 
 %% bar plot for separabilities of models
-    % bar colors
-    bar_colors(1,:) = [0.5 0.5 0.5];
-    bar_colors(2,:) = [247, 148, 30]/255;
-    bar_colors(3,:) = [247, 192, 30]/255;
-    bar_colors(4,:) = [193, 25, 47]/255;
-
-    % make plot
-    datadir = '~/Projects/limblab/data-td/ForceKin/Results/separability';
-    filenames = {'Han_20170203_actpasSeps_run20180818.mat','Chips_20170913_actpasSeps_run20180818.mat'};
-    num_monks = length(filenames);
-    monk_x = (2:3:((num_monks-1)*3+2))/10;
-    template_x = linspace(-0.5,0.5,4)/10;
-    model_spacing = mode(diff(template_x));
-    figure('defaultaxesfontsize',18)
-    for monkeynum = 1:num_monks
-        load(fullfile(datadir,filenames{monkeynum}))
-
-        % calculate stats
-        mean_seps = mean(seps{:,:});
-        var_seps = var(seps{:,:});
-        correction = 1/(num_folds*num_repeats) + 1/(num_folds-1);
-        std_err_seps = sqrt(var_seps*correction);
-
-        for modelnum = 1:length(mean_seps)
-            xval = monk_x(monkeynum) + template_x(modelnum);
-            bar(xval,mean_seps(modelnum),model_spacing,'facecolor',bar_colors(modelnum,:),'edgecolor','none')
-            hold on
-            plot([xval xval],[mean_seps(modelnum)-std_err_seps(modelnum) mean_seps(modelnum)+std_err_seps(modelnum)],'k','linewidth',3)
-        end
-    end
-    plot([0 monk_x(end)+0.2],[0.5 0.5],'--k','linewidth',2)
-    plot([0 monk_x(end)+0.2],[1 1],'--k','linewidth',2)
-    set(gca,'box','off','tickdir','out',...
-        'xtick',monk_x,'xticklabel',filenames,'xlim',[0 monk_x(end)+0.2],...
-        'ytick',[0 0.5 1],'yticklabel',{'','50%','100%'},...
-        'ticklabelinterpreter','none')
-
+%     % bar colors
+%     bar_colors(1,:) = [0.5 0.5 0.5];
+%     bar_colors(2,:) = [247, 148, 30]/255;
+%     bar_colors(3,:) = [247, 192, 30]/255;
+%     bar_colors(4,:) = [193, 25, 47]/255;
+% 
+%     % make plot
+%     datadir = '~/Projects/limblab/data-td/ForceKin/Results/separability';
+%     filenames = {'Han_20170203_actpasSeps_run20180818.mat','Chips_20170913_actpasSeps_run20180818.mat'};
+%     num_monks = length(filenames);
+%     monk_x = (2:3:((num_monks-1)*3+2))/10;
+%     template_x = linspace(-0.5,0.5,4)/10;
+%     model_spacing = mode(diff(template_x));
+%     figure('defaultaxesfontsize',18)
+%     for monkeynum = 1:num_monks
+%         load(fullfile(datadir,filenames{monkeynum}))
+% 
+%         % calculate stats
+%         mean_seps = mean(seps{:,:});
+%         var_seps = var(seps{:,:});
+%         correction = 1/(num_folds*num_repeats) + 1/(num_folds-1);
+%         std_err_seps = sqrt(var_seps*correction);
+% 
+%         for modelnum = 1:length(mean_seps)
+%             xval = monk_x(monkeynum) + template_x(modelnum);
+%             bar(xval,mean_seps(modelnum),model_spacing,'facecolor',bar_colors(modelnum,:),'edgecolor','none')
+%             hold on
+%             plot([xval xval],[mean_seps(modelnum)-std_err_seps(modelnum) mean_seps(modelnum)+std_err_seps(modelnum)],'k','linewidth',3)
+%         end
+%     end
+%     plot([0 monk_x(end)+0.2],[0.5 0.5],'--k','linewidth',2)
+%     plot([0 monk_x(end)+0.2],[1 1],'--k','linewidth',2)
+%     set(gca,'box','off','tickdir','out',...
+%         'xtick',monk_x,'xticklabel',filenames,'xlim',[0 monk_x(end)+0.2],...
+%         'ytick',[0 0.5 1],'yticklabel',{'','50%','100%'},...
+%         'ticklabelinterpreter','none')
+% 
